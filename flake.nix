ecce50ffdca8a0951e4fdc7c0cdf34d2f0f34d5d
migueel15 <miguelangeldorado10@gmail.com>
2025-03-22 19:18:05 +0100

    inital commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Configuración de NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.writeShellScriptBin "bootstrap-nixos" ''
+        set -e  # Salir si hay un error
+
+        DOTFILES_DIR="$HOME/.dotfiles"
+
+        # Clonar el repositorio si no existe
+        if [ ! -d "$DOTFILES_DIR" ]; then
+          echo "Clonando dotfiles en $DOTFILES_DIR..."
+          git clone https://github.com/migueel15/dotfiles.git "$DOTFILES_DIR"
+        else
+          echo "El repositorio de dotfiles ya existe, actualizando..."
+          cd "$DOTFILES_DIR"
+          git pull
+        fi
+
+        echo "Generando configuración de hardware..."
+        sudo nixos-generate-config --force
+
+        echo "Sobrescribiendo configuration.nix con la configuración de flake..."
+        sudo cp "$DOTFILES_DIR/configuration.nix" /etc/nixos/
+
+        echo "Aplicando la configuración..."
+        sudo nixos-rebuild switch --flake "$DOTFILES_DIR"
+
+        echo "Sistema listo con la configuración de dotfiles."
+      '';
+    };
+}
+
